a2ce898d448785de1f7577fe51a2e4742a113746
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-03-09 19:56:40 +1000

    q4 charNum compared to sizeof char

--- workshop2/Q4_charNum/main.c
+++ workshop2/Q4_charNum/main.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 
 
-int main() { 
+/*int main() { 
     char c = 'a';
     printf("%c\n", c); 
     printf("%d\n", c);
+    return 0; 
+}*/
+
+
+int main() { 
+    char c = 'A';
+    printf("sizeof(c) = %u\n", sizeof(c)); 
+    printf("sizeof('A') = %u\n", sizeof('A')); 
+    printf("sizeof(c + c) = %u\n", sizeof(c + c));
     return 0; 
 }
