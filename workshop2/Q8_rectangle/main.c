36841117c5dbb6a15cbb077b3f2daac140bce76f
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-03-09 21:38:22 +1000

    working on rectangle area.
    getting too tired

---
+++ workshop2/Q8_rectangle/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    float height, width;
+	printf("Enter rectangle hieght and width.\n");
+    scanf("%f %f\n", &height, &width);
+    float area = height*width;
+    
+    int rows, colm;
+    for (rows=0;rows<2;rows++){
+        
+        int i = 0;
+        int j = 0;
+        
+        for(colm=0;colm<3;colm++){
+        
+            if ( i == 0) && (rows == 0) {
+                printf("Height \t");
+            } else if (i == 1) && (rows == 0) {
+                printf("Width \t");
+            } else {
+                printf("Area \t");
+            }
+            i = i+1;
+        
+            
+        }
+    rows = 0; 
+    colm = 0;
+    for (rows=0;rows<2;rows++){
+        
+        
+        int j = 0;
+        
+        for(colm=0;colm<3;colm++){
+        
+            if ( j == 0) && (rows == 0) {
+                printf("%f\t", height);
+            } else if (j == 1) {
+                printf("%f\t", width);
+            } else {
+                printf("%\t", area);
+            }
+            j = j+1;
+        
+            
+        }
+    }
+    
+	return 0;
+}
