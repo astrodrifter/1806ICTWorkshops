08fa8abe0c98c0a903fa1516140bc0860a6f7a57
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-03-10 08:29:58 +1000

    finished Q8_rectangle
    was asking for wrong inputs and also creating unnecessary complexity.

--- workshop2/Q8_rectangle/main.c
+++ workshop2/Q8_rectangle/main.c
@@ -1,52 +1,15 @@
+/* take area saure meter and with as 
+ * inputs and print hight sof rectangle*/
 #include <stdio.h>
 
 int main(int argc, char **argv)
 {
-    float height, width;
-	printf("Enter rectangle hieght and width.\n");
-    scanf("%f %f\n", &height, &width);
-    float area = height*width;
+    float area, width;
+	printf("Enter rectangle area in m^2 and width.\n");
+    scanf("%f %f\n", &area, &width);
+    float height = area/width;
+    printf("Height of rectangle = %f m.\n", height);
     
-    /*int rows, colm;
-    for (rows=0;rows<2;rows++){
-        
-        int i = 0;
-        int j = 0;
-        
-        for(colm=0;colm<3;colm++){
-        
-            if ( i == 0) && (rows == 0) {
-                printf("Height \t");
-            } else if (i == 1) && (rows == 0) {
-                printf("Width \t");
-            } else {
-                printf("Area \t");
-            }
-            i = i+1;
-        
-            
-        }
-    rows = 0; 
-    colm = 0;
-    for (rows=0;rows<2;rows++){
-        
-        
-        int j = 0;
-        
-        for(colm=0;colm<3;colm++){
-        
-            if ( j == 0) && (rows == 0) {
-                printf("%f\t", height);
-            } else if (j == 1) {
-                printf("%f\t", width);
-            } else {
-                printf("%\t", area);
-            }
-            j = j+1;*/
-        
-            
-        }
-    }
     
 	return 0;
 }
