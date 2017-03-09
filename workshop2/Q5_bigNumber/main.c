d388a74532e3d1dde2ebccd27bd031cba1561627
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-03-10 08:25:22 +1000

    updated explanations on big number

--- workshop2/Q5_bigNumber/main.c
+++ workshop2/Q5_bigNumber/main.c
@@ -4,4 +4,6 @@ int main() {
     int bigNumber = 2000000000 + 2000000000;
     printf("%d %u\n", bigNumber, bigNumber);
     return 0; 
+    // because signed number is outside range it 
+    // will not print. But unsigned will.
 }
--- workshop2/casting/main.c
+++ workshop2/casting/main.c
@@ -6,7 +6,13 @@ int main(int argc, char **argv)
     float f2 = 4.5;
     int j = f + f2;
     int i = (int) f;
+    
     printf("%f %d\n ", f, i);
     printf("%f % f %d\n ", f, f2, j);
+    
+    int x = 7;
+    int y = 5;
+    double d = x/(double)y;
+    printf("%f \n", d);
 	return 0;
 }
