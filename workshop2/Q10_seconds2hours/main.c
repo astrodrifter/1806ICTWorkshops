5a6f835a2ed7603113a80effa58591920b73b3e9
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-03-11 14:42:15 +1000

    Fixed seconds2Hours
    seconds output was not correct. Now it is.

--- workshop2/Q10_seconds2hours/main.c
+++ workshop2/Q10_seconds2hours/main.c
@@ -14,16 +14,18 @@ int main(int argc, char **argv)
     
     //Find how many hours
     remainderHours = seconds%(60*60);
-    h = (float)seconds/(60*60);
+    h = seconds/(60.00*60);
     hours = (int) h;
     
     //find remainding minutes
-    remianderMinutes = h%60;
-    m = remainderHours/60;
+    remianderMinutes = remainderHours%60;
+    m = remainderHours/60.00;
     minutes = (int) m;
     
     //find remianing seconds
-    remainderSeconds = m%60;
+    remainderSeconds = remianderMinutes%60;
+    //s = remianderMinutes/60.00;
+    //secondsLeft = (int) s;
     
     //print results
 	printf("%i hours, %i minutes and %i seconds.\n", hours, minutes, remainderSeconds);
