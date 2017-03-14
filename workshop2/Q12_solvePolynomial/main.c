e765a184393e78c5d423b4293371961fbfa20af3
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-03-14 12:21:55 +1000

    Tried to solve the nan problem
    I getting an error when I try to use the debugger. It say’s “failed to
    locate gbd at ‘gbd’. I will show my tutor later this week and see if i
    can get it sorted.

--- workshop2/Q12_solvePolynomial/main.c
+++ workshop2/Q12_solvePolynomial/main.c
@@ -6,11 +6,11 @@
 
 int main(int argc, char **argv)
 {
-    float a, b, c, x1, x2;
+    double a, b, c, x1, x2;
     printf("\nEnter values for a, b and c:\n");
-	scanf("%f %f %f", &a, &b, &c);
-    x1 = -b+sqrt(b*b-(2*a*c))/(2*a);
-    x2 = -b-sqrt(pow(b,2)-2*a*c)/(2*a);
+	scanf("%1f %1f %1f", &a, &b, &c);
+    x1 = (-b+(sqrt(pow(b,2)-2*a*c)))/(2*a);
+    x2 = (-b-(sqrt(pow(b,2)-2*a*c)))/(2*a);
     printf("Solution: x1 = %f, x2 = %f.\n\n", x1, x2);
 	//return 0;
 }
