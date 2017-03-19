058edf26a9964730bef9fb8cf9ad788c4d17f906
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-03-19 18:44:47 +1000

    Q4_integerToWord
    now printing digits from left to right but cannot handle negative
    numbers nor num integer inputs.

--- workshop3/Q4_integerToEnglish/main.c
+++ workshop3/Q4_integerToEnglish/main.c
@@ -1,23 +1,83 @@
 /*takes a integer number and prints each didgit in english*/
 
 #include <stdio.h>
-int getNum(int);
-void printWords(int);
+#include <math.h>
 
-int getNum(int n) 
-{
-    printf("Eneter an integer value:\t");
+int howManyDigits(int);
+int getNum(int);
+void printWords(int, int);
+
+
+int howManyDigits(int digits) {
+    printf("How many didgits will your number have?\n");
+    scanf("%i", &digits);
+    return digits;
+}
+
+
+int getNum(int n) {
+    printf("Eneter an integer value:\n");
     scanf("%i", &n);
     return n;
 }
 
-void printWords(int num) 
-{
-    int t = num;
-    int remainder;
+
+void printWords(int num, int digits) {
+    int index = digits-1;
+    int toWord;
     
+    while (index >= 0) {
+          toWord = num/(pow(10.0,index));
+          
+            //if(toWord !=0) {
+                
+                switch(toWord){
+                    case 0:
+                        printf("Zero ");
+                        break;
+                    case 1:
+                        printf("One ");
+                        break;
+                    case 2:
+                        printf("Two ");
+                        break;
+                    case 3:
+                        printf("Three ");
+                        break;
+                    case 4:
+                        printf("Four ");
+                        break;
+                    case 5:
+                        printf("Five ");
+                        break;
+                    case 6:
+                        printf("Six ");
+                        break;
+                    case 7:
+                        printf("Seven ");
+                        break;
+                    case 8:
+                        printf("Eight ");
+                        break;
+                    case 9:
+                        printf("Nine ");
+                    break;
+                    
+                    
+                }
+                num = num - toWord * pow(10.0,index);
+                index--;
+            //}
+        
+    }
+    printf("\n");
+        
+} 
+    /*int t = num;
+    
+    printf(" you selcted %i digits.\n", digits);
     // mach each didgit to english word
-    // use a while loop that gets each digit with %
+    
     while (t != 0) {
         remainder = t % 10;
         switch(remainder){
@@ -54,17 +114,19 @@ void printWords(int num)
       }
       t = t / 10;
    }
-   printf("\n");
-}
+   printf("\n");*/
+//}
 
 
 int main(int argc, char **argv) {  
     
-    
+    //get number of digits
+    int digits = 0;
+    digits = howManyDigits(digits);
     // get user integer
     int num = 0;
     num = getNum(num);
-    printWords(num);
+    printWords(num, digits);
     
 	return 0;
 }
