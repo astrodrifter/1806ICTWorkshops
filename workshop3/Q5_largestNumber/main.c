245df957d2d4dac98023e6ded9ef16121a100f98
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-03-20 15:41:59 +1000

    Q5_largestNumber
    working code complete. takes a user input array and compares elements
    to find the largest.

--- workshop3/Q5_largestNumber/main.c
+++ workshop3/Q5_largestNumber/main.c
@@ -1,23 +1,36 @@
 /*find largest number of entered values*/
 #include <stdio.h>
 
-void largest(void);
+void findMax(void);
 
-
-
-void largest(void){
-    //number in
-    int value = 1;
-    while(1){
-        printf("Enter integer value %i.", value);
-        scanf("%i",)
-        value++;
-    //numbers compared
-    
+int main(int argc, char **argv) {
+    findMax();
+	return 0;
 }
 
-int main(int argc, char **argv)
-{
-	printf("hello world\n");
-	return 0;
+
+void findMax(void) {
+    
+   
+    //get input
+    int howMany; //ask how many numbers to compare
+    printf("How many integers will you compare? ");
+    scanf("%i", &howMany);
+    
+    //create array and loop inputs
+    int myArray[howMany];
+    for(int i = 0; i < howMany; i++){
+        printf("Enter number %i: ",i);
+        scanf("%i", &myArray[i]);
+    }
+    
+    //find largest
+    int max = myArray[0];
+    for(int i = 1; i < howMany; i++){
+        if(myArray[i] > max) {
+            max = myArray[i];
+        }
+    }
+    printf("Your max number is %i.\n",max);
+    // print result
 }
