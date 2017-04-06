e5e987627f9fbc9a6dcfce91a315306016c6326b
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-04-07 09:43:25 +1000

    created golf game

--- workshop5arraysLaptop/Q10_golfGame/main.c
+++ workshop5arraysLaptop/Q10_golfGame/main.c
@@ -4,5 +4,9 @@ int main(int argc, char **argv)
 {
 	printf("Commit to git hub er day yo!\n");
     printf("Github is life yo!\n");
+    
+    char ch = 'A';
+    printf("%p\n",&ch); //hex adress
+    printf("%u\n",&ch); //decimal address
 	return 0;
 }
