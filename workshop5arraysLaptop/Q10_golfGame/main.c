20ccb8b8696ac99771a28dbcd44391782936cc6e
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-04-07 10:15:38 +1000

    golf game
    now has a rand parScore array and random 2D players scores array.

--- workshop5arraysLaptop/Q10_golfGame/main.c
+++ workshop5arraysLaptop/Q10_golfGame/main.c
@@ -1,12 +1,77 @@
+/* Golf game. You and two friends. randomly generate input scores for each hole.
+ * At the end of each game add up score and report various statistics for each player.
+ * Use rand() to genrate score for ech hole (0 < score < 11) 
+ * Use arrays to store scores
+ * A 1D array should be used to store par score for each hole
+ * Use rand() to generate par for each hole (3 < = par < = 5)
+ * Use 3-by-18 2D array for you and two other players.
+ * Then report statistics...
+ * 
+ * Dhruva O'Shea created on 07/04/2017
+ * 
+ * 1. rand() a 1D array par for 18 hole
+ * 2. rand() a 3x18 2D array of palyer scores
+ * 3. sum each players scores
+ * 4. report
+ * */
+ 
 #include <stdio.h>
-
+int HOLES = 18;
+int PLAYERS = 3;
 int main(int argc, char **argv)
 {
-	printf("Commit to git hub er day yo!\n");
-    printf("Github is life yo!\n");
+    // variables
+    int parScores[HOLES], playerScores[PLAYERS][HOLES];
     
+	//1. rand() a 1D array par for 18 hole
+    for(int i = 0; i < HOLES; i++)
+    {
+        int r = rand()%10;
+        if (r == 0)
+        {
+            r = rand()%10;
+        }
+        parScores[i] = r;
+        
+    }
+    
+    // print par scores (testing)
+    for(int i = 0; i < HOLES; i++)
+    {
+        
+        printf("%i ",parScores[i]);
+    }
+    printf("\n");
+    
+    //2. generate 2D array player scores
+    for(int i = 0; i < PLAYERS; i++)
+    {
+        for(int j = 0; j < HOLES; j++)
+        {
+            int r = rand()%10;
+            if (r == 0)
+            {
+                r = rand()%10;
+            }
+            playerScores[i][j] = r;
+        }
+    }
+    
+    //print playerscores (testing)
+    for(int i = 0; i < PLAYERS; i++)
+    {
+        for(int j = 0; j < HOLES; j++)
+        {
+            printf("%i ",playerScores[i][j]);
+        }
+        printf("\n");
+    }
+    
+    /* address
     char ch = 'A';
     printf("%p\n",&ch); //hex adress
     printf("%u\n",&ch); //decimal address
+     */
+     
 	return 0;
 }
