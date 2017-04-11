d5bcdfbbe70cb16d28c114b8c436decd8b43df45
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-04-11 19:17:51 +1000

    created a repo for pointer examples wk 5 lecture

---
+++ workshop5arraysLaptop/wk5Pointers/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    address
+    char ch = 'A';
+    printf("%p\n",&ch); //hex adress
+    printf("%u\n",&ch); //decimal address
+
+	printf("hello world\n");
+	return 0;
+}
