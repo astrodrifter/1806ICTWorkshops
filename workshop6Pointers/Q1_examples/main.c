7467c8145f07de103362a2c2148c319c880d745f
Dhruva O'Shea <dhruva.oshea@connect.qut.edu.au>
2017-04-17 12:43:29 +1000

    Workshop 6 Pointers repo created

---
+++ workshop6Pointers/Q1_examples/main.c
@@ -0,0 +1,22 @@
+/*1. Write a C program that performs each of the following tasks:
+(a) Declare and initialise next to be a character variable with the value 'B'.
+(b) Declare and initialise current to be a character variable with the value 'y'.
+(c) Declare ptr to be a pointer to objects of type char.
+(d) Assign the address of current to the variable ptr.
+(e) Change the value of the object pointed to by ptr to '0'.
+(f) Assign the address of next to the variable ptr.
+(g) Change the value of the object pointed to by ptr to 'd'.
+(h) Print the address stored in ptr.
+(i) What values are stored in next and current? 
+ * 
+ * Dhruva O'Shea 17/04/17
+ * 
+ * */
+ 
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("hello world\n");
+	return 0;
+}
