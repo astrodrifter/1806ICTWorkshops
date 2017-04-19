6b7bd37af545acbb183bfe9f8c0bc551e4de73d0
Dhruva O'Shea <dhruva.oshea@connect.qut.edu.au>
2017-04-19 13:01:53 +1000

    pages 0-14 lecture

--- week7Strings/StringsLectureExamples/main.c
+++ week7Strings/StringsLectureExamples/main.c
@@ -2,6 +2,17 @@
 
 int main(int argc, char **argv)
 {
-	printf("hello world\n");
+	// last charetor is \0 pages 10-12
+	char name[5] = "Ann"; // 5 cells one empty
+	char name2[] = "Ann"; // 4 sell used
+	
+	//print dont nee loop
+	printf("%s\n",name);
+	printf("%c%c%c\n",name[0],name[1],name[2]);
+	name[0] = 'B';
+	printf("%s\n",name);
+	printf("%c%c%c\n",name[0],name[1],name[2]);
+	
+	char *name = "Ann"; // cannot chnage this now. It is a constant.
 	return 0;
 }
