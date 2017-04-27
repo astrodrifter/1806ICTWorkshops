488db2c42a77744a9986730f04aca20ab12b8033
Dhruva O'Shea <dhruva.oshea@connect.qut.edu.au>
2017-04-27 17:55:57 +1000

    lowerToUpper solved
    git commits everyday

--- week7Strings/lowerToUpper/main.c
+++ week7Strings/lowerToUpper/main.c
@@ -10,50 +10,44 @@
 #include <stdio.h>
 #include <string.h>
 #define MAXLENGTH 50
+
 int main(int argc, char **argv)
-{
+	{
 	/* get String */
-	char word[MAXLENGTH];
+	char word[MAXLENGTH],word1[MAXLENGTH];
 	printf("Enter a string\n");
 	scanf("%s",word);
 	printf(" Your string is '%s'\n",word);
-	
-	/* using toupper()*/
+	strcpy(word1,word);
+	/* using toupper() and tolower() */
 	int i = 0;
 	while(word[i] != 0 && word[i])
 	{
 		if (word[i] >= 'A' && word[i] <= 'Z')
-		{
-			word[i] = (tolower(word[i]));
-		} else {
-			word[i] = (toupper(word[i]));
-		}
-		i++;
-	}
-	printf(" Your new string is '%s'\n",word);
-	
-	/* test *//*
-	char ch = 'A';
-	int ich = (int)ch;
-	printf("%i\n",ich);
-	*/ 
-	int j = 0;
-	while(word[j] != 0 && word[j])
 	{
-		if (word[j] >= 'A' && word[j] <= 'Z')
+		word[i] = (tolower(word[i]));
+	} else {
+		word[i] = (toupper(word[i]));
+	}
+	i++;
+	}
+	printf(" Your new string With Functions is '%s'\n",word);
+
+
+	/* without string functions */
+	int j = 0;
+	while(word1[j] != 0 && word1[j])
+	{
+		if (word1[j] >= 'A' && word1[j] <= 'Z')
 		{
-			//printf("Character is Upper Case Letters\n");
-			int i = ((int)word[j])+32;
-			word[j] = i + '0';
+			int i = ((int)word1[j])+32;
+			word1[j] = i + '0';
 		} else {
-			//printf("Character is Not Upper Case Letters\n");
-			int i = ((int)word[j])-80;
-			word[j] = i + '0';
+			int i = ((int)word1[j])-80;
+			word1[j] = i + '0';
 		}
 		j++;
 	}
-   
-	printf(" Your new string is '%s'\n",word);
-	/* find ASCI table. Upper case are only a set number away from lower */
+	printf(" Your new string Without Functions is '%s'\n",word);
 	return 0;
 }
