b97f82a68d126e250a20e565643f5e836eadab5f
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-04-30 08:18:43 +1000

    edited wrong file. now fixed

--- week7StringLaptop/palindrome2/main.c
+++ week7StringLaptop/palindrome2/main.c
@@ -6,9 +6,52 @@
    * 
 */
 #include <stdio.h>
+#include <string.h>
+
+#define MAXLEN 100
+
+void removeSpace(char *str, char *strCondenced)
+{
+    int i;
+    int j = 0;
+    
+    for(i = 0; i < strlen(str); i++)
+    {
+        if(str[i] == ' ')
+        {
+            i++;
+        }
+        strCondenced[j] = str[i];
+        j++;
+    }
+}
+void reverseString(char *str, char *reversedStr)
+{
+    int i;
+    for (i=strlen(str)-1; i>=0; i--)
+    {
+        *reversedStr++ = *(str+i);
+    }
+        *reversedStr = '\0';
+}
 
 int main(int argc, char **argv)
-{
-	printf("hello world\n");
-	return 0;
+    {
+    char str[MAXLEN];
+    char strCondenced[MAXLEN];
+    char reversedStr[MAXLEN];
+    while (fgets(str, sizeof(str), stdin) != NULL)
+    {
+        str[strlen(str)-1] = '\0';   // the last character is the newline. Replace with null
+        removeSpace(str, strCondenced);
+        printf(" space romved  = %s\n",strCondenced);
+        reverseString(strCondenced, reversedStr);
+        printf(" space romved reverse  = %s\n",reversedStr);
+        if (strcmp(strCondenced, reversedStr) == 0)
+            printf("%s is a palindrome\n", str);
+        else
+            printf("%s is not a palindrome\n", str);
+    }
+    return 0; 
+
 }
