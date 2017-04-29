dd1dbbe43fd07cf13eb2697605266d8bac6628f9
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-04-30 08:14:51 +1000

    palindrome2 working solution
    takes a sentence and checks if palindrome. handles spaces but does
    handle difference in capitals yet.

--- week7StringLaptop/palindrom/main.c
+++ week7StringLaptop/palindrom/main.c
@@ -15,6 +15,21 @@
 
 #define MAXLEN 100
 
+void removeSpace(char *str, char *strCondenced)
+{
+    int i;
+    int j = 0;
+    
+    for(i = 0; i < strlen(str); i++)
+    {
+        if(str[i] == ' ')
+        {
+            i++;
+        }
+        strCondenced[j] = str[i];
+        j++;
+    }
+}
 void reverseString(char *str, char *reversedStr)
 {
     int i;
@@ -28,12 +43,16 @@ void reverseString(char *str, char *reversedStr)
 int main(int argc, char **argv)
     {
     char str[MAXLEN];
+    char strCondenced[MAXLEN];
     char reversedStr[MAXLEN];
     while (fgets(str, sizeof(str), stdin) != NULL)
     {
         str[strlen(str)-1] = '\0';   // the last character is the newline. Replace with null
-        reverseString(str, reversedStr);
-        if (strcmp(str, reversedStr) == 0)
+        removeSpace(str, strCondenced);
+        printf(" space romved  = %s\n",strCondenced);
+        reverseString(strCondenced, reversedStr);
+        printf(" space romved reverse  = %s\n",reversedStr);
+        if (strcmp(strCondenced, reversedStr) == 0)
             printf("%s is a palindrome\n", str);
         else
             printf("%s is not a palindrome\n", str);
---
+++ week7StringLaptop/palindrome2/main.c
@@ -0,0 +1,14 @@
+/*Modify your program in Q10 so that blanks and capital letters are ignored in the 
+  matching process. Under these new rules, the following are examples of palindromes:
+  “Anna” “a man a plan a canal Panama” “Amy must I jujitsu my ma”
+   * 
+   * Dhruva O'Shea 30/04/17
+   * 
+*/
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("hello world\n");
+	return 0;
+}
