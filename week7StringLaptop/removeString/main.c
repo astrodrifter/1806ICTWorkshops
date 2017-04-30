7835cd4f815e687dece4016658e192a8422e02e1
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-04-30 21:39:32 +1000

    clean up findString

--- week7StringLaptop/removeString/main.c
+++ week7StringLaptop/removeString/main.c
@@ -9,9 +9,11 @@
 
 #define MAXLEN 100
 
-/* takes two str1 and str2 and checks if str2 is in str1 */ 
 int findString(char str1[], char str2[])
 {
+    printf("str1 = %s\n",str1);
+    printf("str2 = %s\n",str2);
+    printf("str2 length = %i\n",strlen(str2));
     /* Loop through first letter to first letter
     * if first letter found check rest */
     int i, place = 0, count = 0;
@@ -20,12 +22,15 @@ int findString(char str1[], char str2[])
         if(str2[0] == str1[i])
         {
             place = i;
+            printf("place  = %i\n",place);
             int j;
             for(j=i; j<strlen(str2)+i; j++)
             {
+                printf("j = %i\n",j);
                 if(str2[count] == str1[j])
                 {
                     count++;
+                    printf("count = %i\n",count);
                 }
             }
         }
@@ -41,6 +46,7 @@ int main(int argc, char **argv)
 {
     int place;
     place = findString("a chatter box", "hat");
+    
 	printf("Possition is %i\n",place);
 	return 0;
 }
