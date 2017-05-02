27dc02e8db3843583852efe29448de2f164e7e1b
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-05-02 12:48:43 +1000

    saving. having some problems like some keys are helps down.

--- week7StringLaptop/insertString/main.c
+++ week7StringLaptop/insertString/main.c
@@ -10,31 +10,55 @@
 
 #define MAXLEN 200
 
-void insertString(char str1[], char str2[], int pos)
+int insertString(char str1[], char str2[], int pos)
 {
-    /* start from pos
+    /* get lengths of both strings */
+    int lengthStr1,lengthStr2;
+    lengthStr1 = strlen(str1);
+    lengthStr2 = strlen(str2);
+    
+    
+    /*
+     * start from pos
      * copy from str1[pos] to string[pos+strlen(str2)] to indexes > str1[strlen(str1)]
      * enter str2 over indexes from str1[pos] to string[pos+strlen(str2)]
      */
     int i, j = 0;
-    for(i = pos; i < strlen(str1) + strlen(str2); i++, j++)
+    for(i = pos; i < lengthStr1 + lengthStr2; i++, j++)
     {
-        str1[strlen(str1) + j] = str1[i];      //copy inside elements to outside original str1
-        str1[i] = str2[j];                     //copy str2 inside str1 
+        str1[lengthStr1 + j] = str1[i];      //copy inside elements to outside original str1
+        //str1[i] = str2[j];                     //copy str2 inside str1 
     }
-}
+    
+    /* try another string */
+    int test;
+    printf("test another string? 0 for No, 1 for yes\n");
+    scanf("%i",test);
+    if(test == 0)
+    {
+        return 0;
+    } else {
+        return 1;
+    }
+}   
+
 int main(int argc, char **argv)
 {
-    char str1[MAXLEN], str2[MAXLEN];
-    for(int i = 0; i < MAXLEN; i++)
+    char str1[MAXLEN],str2[MAXLEN]; 
+    int pos, test = 1;
+    
+    printf("Enter your first string\n");
+    scanf("%s",str1);
+    printf("Enter your second string\n");
+    scanf("%s",str2);
+    printf("Enter postion to insert\n");
+    scanf("%i",&pos);
+    
+    while(test)
     {
-        str1[i] = '\0';
-        str2[i] = '\0';
+        test = insertString(str1, str2, pos);
+        printf("new str1 is '%s'\n",str1);
     }
-    str1 = "The wrong son";
-    str2 = "per";
-    int pos = 10;
-    insertString(str1, str2, pos);
-    printf("new str1 is '%s'\n",str1);
+    
 	return 0;
 }
