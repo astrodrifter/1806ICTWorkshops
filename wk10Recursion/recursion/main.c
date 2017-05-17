07f180211a04873e40be4223f32e03dbb61ed1f5
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-05-17 20:31:43 +1000

    comments

--- wk10Recursion/recursion/main.c
+++ wk10Recursion/recursion/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/* Main program for testing factorial() function */
+/*calculates and returns factorial of n */
 int factorial(int n)
 {
     if( n <= 1){
@@ -10,9 +10,19 @@ int factorial(int n)
     }
 }
 
+/* calculates and returns fobonacci of n */
+int fibonacci(int n)
+{
+    if(n <= 1)
+        return n;
+    else 
+        return fibonacci(n - 2) + fibonacci(n - 1);
+}
+/* Main program for testing factorial() function */
 int main(void) {
     int n;
     printf("Please enter n: "); scanf("%d", &n);
     printf("%d! is %d\n", n, factorial(n));
+    printf("fibonacci of %d is %d\n", n, fibonacci(n));
     return 0;
 }
