1deb284aa269d7b27bb04fba574cd07a4e1b6d1f
Dhruva O'Shea <dhruva.oshea@connect.qut.edu.au>
2017-05-24 21:45:46 +1000

    started a greatest common denominator recursive function but need to sleep. Not finished.

---
+++ wk10Recursion/gcd/main.c
@@ -0,0 +1,24 @@
+/* use recursion to make a power function */
+/* Dhruva O'Shea 24/05/17 */
+#include <stdio.h>
+
+int gcd(int x, int y, int n) {
+	int n = 1;
+	if(y == 0 || x == 0) {
+		return 0;
+	} else if ( x%n == 0 && y%n == 0 ) {
+		return 
+	}
+}
+
+int main(int argc, char **argv)
+{
+	int x, y n = 0 gCd;
+	printf("Enter x and y values\n");
+	scanf("%i",&x);
+	scanf("%i",&y);
+	gCd = gcd(x,y,n);
+
+	
+	return 0;
+}
