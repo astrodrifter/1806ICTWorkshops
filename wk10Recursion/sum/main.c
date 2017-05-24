ed580755f2eee37023ab318671ac489ea99a1f25
Dhruva O'Shea <dhruva.oshea@connect.qut.edu.au>
2017-05-24 20:24:55 +1000

    sum recursion

---
+++ wk10Recursion/sum/main.c
@@ -0,0 +1,16 @@
+/* Write a recursive function that computes the sum of the first n 
+ * positive integers.Your program will read in the integer value 
+ * for n, and call the recursive function to compute the 
+ * sum of 1+2+3+...+ n. 
+ * 
+ * Dhruva O'Shea 24/05/17
+ * 
+*/
+ 
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("hello world\n");
+	return 0;
+}
