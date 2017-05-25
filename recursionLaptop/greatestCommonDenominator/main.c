8ab6bb288790aa44c2f69bbb4288201b3640f347
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-05-25 20:42:52 +1000

    gcd recursion

---
+++ recursionLaptop/greatestCommonDenominator/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+// gcd recursively finds greatest common divisor of x and y 
+int gcd(int x, int y)
+{
+    int check;
+     // base case
+     if (y == 0)
+     {
+return x; }
+     // recursive step
+     else
+     {
+         return check;
+     }
+}
+
+int main() {
+    int x; // first integer
+    int y; // second integer
+    int gcDiv; // greatest common divisor of x and y
+    printf("%s", "Enter two integers: ");
+    scanf("%u%u", &x, &y);
+    gcDiv = gcd(x, y);
+    printf("Greatest common divisor of %u and %u is %u\n", x, y, gcDiv);
+}
