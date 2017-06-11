f555d163552599fee95c1919a0be427e41f3220e
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-06-11 21:38:24 +1000

    arraySum now has input validation

--- examPractice/array1/main.c
+++ examPractice/array1/main.c
@@ -3,21 +3,23 @@
 
 int main(int argc, char **argv)
 {
-    int size, sum = 0;
-	printf("Enter array size and then elements, all integers\n");
+    int size, status, temp, sum = 0;
+    printf("Enter array size and then elements, all integers\n");
     scanf("%i",&size);
     int myArray[size];
     for(int i = 0; i < size; i ++) {
-        scanf("%i",&myArray[i]);
-        if(myArray[i]) 
-            printf("int\n");
-        else
-            printf("not int\n");
+        status = scanf("%i",&myArray[i]);
+        while(status!=1){
+            while((temp=getchar()) != EOF && temp != '\n');
+            printf("Array[%i] was invalid input... \nEnter it again along with all trailing elements: \n",i);
+            status = scanf("%d", &myArray[i]);
+        }
     }
     
     for(int i = 0; i < size; i ++) {
-        printf("%i ",myArray[i]);
+        sum+=myArray[i];
     }
     
+    printf("Sum = %i\n",sum);
 	return 0;
 }
