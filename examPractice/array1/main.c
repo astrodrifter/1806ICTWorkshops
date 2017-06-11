3d8807c6756185e23e5414267d48e1f2e7c938de
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-06-11 21:10:00 +1000

    array1 started

---
+++ examPractice/array1/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main(int argc, char **argv)
+{
+    int size, sum = 0;
+	printf("Enter array size and then elements, all integers\n");
+    scanf("%i",&size);
+    int myArray[size];
+    for(int i = 0; i < size; i ++) {
+        scanf("%i",&myArray[i]);
+    }
+    
+    for(int i = 0; i < size; i ++) {
+        printf("%i ",&myArray[i]);
+    }
+    
+	return 0;
+}
