8def9f8ccca50bfca8427df70a2aa965acc3e50f
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-06-12 19:36:48 +1000

    more pointing

--- examPractice/ptr1/main.c
+++ examPractice/ptr1/main.c
@@ -7,8 +7,14 @@ int main(int argc, char **argv)
     myPtr = &myNum; //myPtr = address of myNum
     printf("myNum = %i\n",myNum); //myNum = 7
     printf("myPtr = %i\n",myPtr); //address of myPtr
-    printf("*myPtr = %i\n",*myPtr); //contents of what myPtr is pointing to (myNum)
+    printf("*myPtr = %i\n",*myPtr); //contents of what myPtr is pointing to (myNum) "dereferencing"
     printf("&myPtr = %i\n",&myPtr); //contents of myPtr
+    *myPtr = 5; //changed contents of myNum to 5
+    printf("myNum = %i\n",myNum); 
+    printf("myPtr = %i\n",myPtr); 
+    printf("*myPtr = %i\n",*myPtr); 
+    printf("&myPtr = %i\n",&myPtr);
+    
     
 	return 0;
 }
