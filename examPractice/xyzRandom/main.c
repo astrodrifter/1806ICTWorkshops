73dbede2d227c955ebb0916727666b0c979ac5d2
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-06-14 22:10:53 +1000

    xyzRandom 0 < x < 10 1 decimal place

--- examPractice/xyzRandom/main.c
+++ examPractice/xyzRandom/main.c
@@ -9,9 +9,9 @@ int main(int argc, char **argv)
     double x, y, z;
     for(int i = 0; i < 100; i++) {
         x = rand()%10;
-        y = rand();
-        z = rand();
-        printf("%f %f %f\n",x,y,z);
+        y = rand()%10;
+        z = rand()%10;
+        printf("%.1f %.1f %.1f\n",x,y,z);
     }
 	printf("You're Welcome\n");
 	return 0;
