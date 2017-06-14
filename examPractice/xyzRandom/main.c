036beb5c4f68fb80fcd0f56cf897c78eaee6007f
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-06-14 22:24:12 +1000

    xyzRandom now has for different ranges

--- examPractice/xyzRandom/main.c
+++ examPractice/xyzRandom/main.c
@@ -7,11 +7,33 @@ int main(int argc, char **argv)
 {
     srand(time(NULL));   // should only be called once
     double x, y, z;
+    
     for(int i = 0; i < 100; i++) {
-        x = rand()%10;
+        x = rand(); // numbers bewteen 0 and RAND_MAX
+        y = rand();
+        z = rand();
+        printf("%.1f %.1f %.1f\n",x,y,z);
+    }
+    
+    for(int i = 0; i < 100; i++) {
+        x = rand()%100; // umbers bewteen 0 and 100
+        y = rand()%100;
+        z = rand()%100;
+        printf("%.1f %.1f %.1f\n",x,y,z);
+    }
+    
+    for(int i = 0; i < 100; i++) {
+        x = rand()%10; // Numbers bewteen 0 and 10
         y = rand()%10;
         z = rand()%10;
         printf("%.1f %.1f %.1f\n",x,y,z);
+    }
+    
+    for(int i = 0; i < 100; i++) {
+        x = rand()/(double)RAND_MAX; // Numbers bewteen 0 and 1
+        y = rand()/(double)RAND_MAX;
+        z = rand()/(double)RAND_MAX;
+        printf("%.3f %.3f %.3f\n",x,y,z);
     }
 	printf("You're Welcome\n");
 	return 0;
