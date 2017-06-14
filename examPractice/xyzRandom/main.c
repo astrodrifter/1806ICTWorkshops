2274ddc20c52a0fea8100fc394a8c6f30902cebd
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-06-14 22:09:14 +1000

    xyzRandom Generator

--- examPractice/xyzRandom/main.c
+++ examPractice/xyzRandom/main.c
@@ -8,10 +8,10 @@ int main(int argc, char **argv)
     srand(time(NULL));   // should only be called once
     double x, y, z;
     for(int i = 0; i < 100; i++) {
-        x = rand();
+        x = rand()%10;
         y = rand();
         z = rand();
-        printf("%.2f %.2f %.2f\n",x,y,z);
+        printf("%f %f %f\n",x,y,z);
     }
 	printf("You're Welcome\n");
 	return 0;
